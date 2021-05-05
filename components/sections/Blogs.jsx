a276ad53d1190bd5b47a49fb0eec64fb1f46d10d
Adithya Sreyaj <adi.sreyaj@gmail.com>
2021-05-05 22:13:19 +0530

    feat(blogs): add view more button

--- components/sections/Blogs.jsx
+++ components/sections/Blogs.jsx
@@ -7,9 +7,19 @@ const Blogs = ({ posts }) => {
     <section className="container site-section" id="blog">
       <SectionHeading heading="Blog" subheading="posts" />
       <div className="relative grid grid-cols-1 gap-6 mt-10 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {posts.slice(0, 6).map((post) => (
+        {posts.slice(0, 4).map((post) => (
           <BlogCard post={post} key={post.cuid} />
         ))}
+      </div>
+      <div className="flex justify-center mt-10">
+        <a
+          href="https://blog.sreyaj.dev"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="button primary"
+        >
+          View More
+        </a>
       </div>
     </section>
   );
